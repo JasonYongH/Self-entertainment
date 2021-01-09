cad34834fa3c70b98980950f46c5c96236a1dfef
Jason <hymicro_led@163.com>
2021-01-09 15:33:52 +0800

    [LEETCODE] 2021/1/9 sum two digits represented by list

--- HelloDate.java
+++ HelloDate.java
@@ -21,5 +21,61 @@ public class HelloDate {
         {
             System.out.print(rod.nextInt(100) + " ");
         }
+
+class shape{
+    shape() {
+        System.out.println("this is shape constructor");
     }
+}
+
+class circle extends shape{
+    circle() {
+        System.out.println("this is circle constructor");
+    }
+}
+
+public class HelloDate {
+    int count = 1;
+    public static void main(String[] args){
+        // System.out.print("Hello, it's: ");
+        // System.out.println(new Date());
+        // person Jason = new person();
+
+        // float f[] = new float[10];
+        // Random rod = new Random();
+        // for (int i = 0; i < 10; ++i)
+        // {
+        //     f[i] = rod.nextFloat();
+        // }
+        // for (float x: f)
+        // {
+        //     System.out.print(x + " ");
+        // }
+        // label1:
+        // for (int i = 0; i < 3; i++)
+        // {
+        //     for (int j = 0; j < 3; ++j)
+        //     {
+        //         System.out.println("i is: " + i + ", j is: " + j);
+        //         if (1 == j)
+        //         {
+        //             break label1;
+        //         }
+        //     }
+        // }
+        // circle tes = new circle();
+        // tes = null;
+        // System.gc();
+        Day day = Day.FRIDAY;
+        if (day == Day.FRIDAY)
+        {
+            System.out.println("successful test for enum class");
+        }
+        shape tos = new shape();
+        System.out.println(tos);
+    }
+}
+
+enum Day{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
 }
--- class.java
+++ class.java
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 
+=======
+// 2021/1/9=============================================================================
+>>>>>>> 046d5fe ([LEETCODE] 2021/1/9 sum two digits represented by list)
 // Definition for singly-linked list.
 class ListNode {
     int val;
@@ -62,5 +66,105 @@ class Solution {
             cur_node.next = new ListNode(jinwei_val);
         }
         return head_node;
+
+// class Solution {
+//     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//         int temp_val = 0, jinwei_val = 0;
+//         ListNode head_node = null, cur_node = null;
+//         while(null != l1 && null != l2)
+//         {
+//             temp_val = l1.val + l2.val + jinwei_val;
+//             if (null == head_node)
+//             {
+//                 head_node = new ListNode(temp_val);
+//                 cur_node = head_node;
+//             }
+//             else
+//             {
+//                 cur_node.next = new ListNode(temp_val);
+//                 cur_node = cur_node.next;
+//             }
+//             jinwei_val = temp_val/10;
+//             temp_val = temp_val%10;
+//             cur_node.val = temp_val;
+            
+//             l1 = l1.next;
+//             l2 = l2.next;
+//         }
+//         if (null == l1 && null != l2)
+//         {
+//             while (null != l2)
+//             {
+//                 temp_val = l2.val + jinwei_val;
+//                 jinwei_val = temp_val/10;
+//                 temp_val = temp_val % 10;
+
+//                 cur_node.next = new ListNode(temp_val);
+//                 cur_node = cur_node.next;
+//                 l2 = l2.next;
+//             }
+//         }
+//         if (null == l2 && null != l1)
+//         {
+//             while (null != l1)
+//             {
+//                 temp_val = l1.val + jinwei_val;
+//                 jinwei_val = temp_val/10;
+//                 temp_val = temp_val % 10;
+
+//                 cur_node.next = new ListNode(temp_val);
+//                 cur_node = cur_node.next;
+//                 l1 = l1.next;
+//             }
+//         }
+//         if (0 != jinwei_val)
+//         {
+//             cur_node.next = new ListNode(jinwei_val);
+//         }
+//         return head_node;
+//     }
+// }
+
+//version 2.0
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int n1 = 0, n2 = 0;
+        int temp_val = 0, jinwei_val = 0;
+        ListNode head = null, tail = null;
+        while (null != l1 || null != l2)
+        {
+            n1 = null != l1? l1.val: 0;
+            n2 = null != l2? l2.val: 0;
+            temp_val = n1 + n2 + jinwei_val;
+            jinwei_val = temp_val / 10;
+
+            if (null == head)
+            {
+                head = new ListNode(temp_val % 10);
+                tail = head;
+            }
+            else
+            {
+                tail.next = new ListNode(temp_val % 10);
+                tail = tail.next;
+            }
+
+            if (null != l1)
+            {
+                l1 = l1.next;
+            }
+            if (null != l2)
+            {
+                l2 = l2.next;
+            }
+        }
+
+        if (0 != jinwei_val)
+        {
+            tail.next = new ListNode(jinwei_val);
+            tail = tail.next;
+        }
+        return head;
     }
 }
