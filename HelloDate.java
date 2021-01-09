8a8b86eb5160fa8fe43ad34a897ba9cdfcdedc10
Jason <hymicro_led@163.com>
2021-01-09 14:18:53 +0800

    [JAVA_PRACTICE] 2021/1/9

---
+++ HelloDate.java
@@ -0,0 +1,9 @@
+import java.util.*;
+/* a test class */
+public class HelloDate {
+    int count = 1;
+    public static void main(String[] args){
+        System.out.print("Hello, it's: ");
+        System.out.print(new Date());
+    }
+}
