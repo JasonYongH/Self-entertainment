9f953ac9cfdbb7c287e5bc329a30da0e291c735d
Jason <hymciro_led@163.com>
2021-01-11 14:50:11 +0800

    [JAVA PRACTICE] practice about subclass

--- top.java
+++ top.java
@@ -1,7 +1,36 @@
 import top.mip;
-public class top {
+class shape{
+    String s = "this is a shape";
+    // public String toString() {return s;}
+    shape(){
+        System.out.println("shape constructor");
+    }
+    void draw() { 
+        System.out.println("shape.draw");
+    }
+}
+
+class circle extends shape{
+    circle(){
+        System.out.println("circle constructor");
+    }
+    void draw() {
+        System.out.println("circle.draw");
+    }
+}
+
+class top {
     public static void main(String[] args){
-        System.out.println("pop");
-        mip.woop();
+        shape tes_shape = new shape();
+        // System.out.println(tes_shape.s);
+        // tes_shape.draw();
+        top.draw_line(tes_shape);
+        tes_shape = new circle();
+        // System.out.println(tes_shape.s);
+        // tes_shape.draw();
+        top.draw_line(tes_shape);
+    }
+    static void draw_line(shape test_shape){
+        test_shape.draw();
     }
 }
